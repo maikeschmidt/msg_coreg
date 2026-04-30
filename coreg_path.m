1481c84c6efbf5fb5761dd4660a212a0856b6d63
Maike Schmidt <168002291+maikeschmidt@users.noreply.github.com>
2026-04-30 10:14:04 +0200

    Update coreg_path.m

--- coreg_path.m
+++ coreg_path.m
@@ -1,3 +1,13 @@
+% Automatically detects the file path for where the msg_coreg
+% repository is saved
+%
+% Copyright (c) 2026 University College London
+% Department of Imaging Neuroscience
+%
+% Author: Maike Schmidt
+% Date: April 2026
+%
+
 function folderpath = coreg_path
 folderpath = fileparts(mfilename('fullpath'));
 end
